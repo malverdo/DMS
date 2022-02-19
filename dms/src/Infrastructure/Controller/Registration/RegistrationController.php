3766bb21a2fdfe99b7ef5b85d92fa52c8b66ba7e
malverdo <malverdo11@mail.ru>
2022-02-19 10:53:36 +0300

    add RegistrationController

---
+++ dms/src/Infrastructure/Controller/Registration/RegistrationController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Infrastructure\Controller\Registration;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class RegistrationController  extends AbstractController
+{
+
+}
