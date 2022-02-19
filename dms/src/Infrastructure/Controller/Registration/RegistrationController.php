e1d629afc2d903e5d637d2dd99532842107db158
malverdo <malverdo11@mail.ru>
2022-02-19 11:04:36 +0300

    get getById

--- dms/src/Infrastructure/Controller/Registration/RegistrationController.php
+++ dms/src/Infrastructure/Controller/Registration/RegistrationController.php
@@ -2,9 +2,29 @@
 
 namespace App\Infrastructure\Controller\Registration;
 
+use App\Infrastructure\Factory\ContainerFactory;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController  extends AbstractController
 {
 
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function registration(
+        Request $request
+    ): Response {
+        $content = $request->getContent();
+
+
+        return $this->json([
+            'message' => 'Welcome to your new RegistrationController!'
+        ]);
+    }
 }
